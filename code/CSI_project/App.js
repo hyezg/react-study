fc920e947537c275c33aca74a1b0c68d6239b192
hyejeong <112006114+hyezg@users.noreply.github.com>
2023-09-25 22:28:41 +0900

    Create App.js

---
+++ code/CSI_project/App.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      <header className="App-header">
+        <div>
+          <div className="title">
+            <h1>CSI</h1>
+            <h3>cs 기술 면접 대비</h3>
+          </div>
+          <div className="small">
+            <p>computer</p>
+            <p>science</p>
+            <p>interview</p>
+          </div>
+        </div>
+      </header>
+
+      <footer>
+        <div className="bottom-bar">
+          <div className="circle">
+              <p>홈</p>
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
